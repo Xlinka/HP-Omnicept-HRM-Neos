ae6a2f4e5e699031597fd08b15439ba9bbd876c1
Oliver Dunnington <Oliver.dunnington@outlook.com>
2023-02-11 02:52:48 +0000

    Update 0.1
    fixed the Code and dll reference so its now compileable

--- HP-Omnicept HRM/Program.cs
+++ HP-Omnicept HRM/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
+﻿using System.Net;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
@@ -14,50 +12,71 @@ namespace ReverbOmnicept
     {
         static void Main(string[] args)
         {
-            GetHeartRateData().Wait();
+            HostHeartRateData().Wait();
         }
 
-        static async Task GetHeartRateData()
+        static async Task HostHeartRateData()
         {
-            using (var webSocket = new ClientWebSocket())
+            var listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Start();
+
+            Console.WriteLine("Listening for WebSocket connections...");
+            while (true)
             {
-                await webSocket.ConnectAsync(new Uri("ws://localhost:8080"), CancellationToken.None);
+                var context = await listener.GetContextAsync();
 
-                while (webSocket.State == WebSocketState.Open)
+                if (context.Request.IsWebSocketRequest)
                 {
-                    HeartRate hr = GetHeartRateFromOmnicept();
-                    int heartRate = Convert.ToInt32(hr.Rate);
-                    string heartRateString = heartRate.ToString();
-
-                    var buffer = Encoding.UTF8.GetBytes(heartRateString);
-                    await webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
-
-                    Console.Write("Heart Rate: ");
-                    if (heartRate > 120)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine(heartRate);
-                        Console.ResetColor();
-                    }
-                    else if (heartRate > 100)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkYellow;
-                        Console.WriteLine(heartRate);
-                        Console.ResetColor();
-                    }
-                    else
-                    {
-                        Console.WriteLine(heartRate);
-                    }
-
-                    string fileName = string.Format("{0}.txt", DateTime.Now.ToString("yyyy-MM-dd"));
-                    using (var logFile = File.AppendText(fileName))
-                    {
-                        logFile.WriteLine("[{0}] Heart Rate: {1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), heartRate);
-                    }
-
-                    Thread.Sleep(1000);
+                    Console.WriteLine("WebSocket request received.");
+                    await HandleWebSocketRequest(context);
                 }
+                else
+                {
+                    Console.WriteLine("Non-WebSocket request received, ignoring.");
+                }
+            }
+        }
+
+        static async Task HandleWebSocketRequest(HttpListenerContext context)
+        {
+            var webSocketContext = await context.AcceptWebSocketAsync(null);
+            var webSocket = webSocketContext.WebSocket;
+
+            while (webSocket.State == WebSocketState.Open)
+            {
+                HeartRate hr = GetHeartRateFromOmnicept();
+                int heartRate = Convert.ToInt32(hr.Rate);
+                string heartRateString = heartRate.ToString();
+
+                var buffer = Encoding.UTF8.GetBytes(heartRateString);
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+
+                Console.Write("Heart Rate: ");
+                if (heartRate > 120)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(heartRate);
+                    Console.ResetColor();
+                }
+                else if (heartRate > 100)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine(heartRate);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine(heartRate);
+                }
+
+                string fileName = string.Format("{0}.txt", DateTime.Now.ToString("yyyy-MM-dd"));
+                using (var logFile = File.AppendText(fileName))
+                {
+                    logFile.WriteLine("[{0}] Heart Rate: {1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), heartRate);
+                }
+
+                Thread.Sleep(1000);
             }
         }
 
@@ -68,14 +87,16 @@ namespace ReverbOmnicept
             try
             {
                 heartRate = Convert.ToInt32(hr.Rate.ToString() + " BPM");
-
             }
+
+
             catch (Exception ex)
             {
                 Console.WriteLine("Error retrieving heart rate data: " + ex.Message);
             }
 
             return hr;
+
         }
     }
 }
